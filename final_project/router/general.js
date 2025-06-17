d3873d660af0de9a2aa343128b34e5357db318cb
Kamran <muhammad.kamran@imaginationai.net>
2025-06-17 17:48:25 +0500

    Completed task 5: express final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -61,8 +61,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const {isbn} = req.params;
+  const book = books[isbn]
+  if(book){
+    return res.status(200).json({review: books[isbn].reviews});
+
+  } else return res.status(500).json({message: "book not found"});
+
 });
 
 module.exports.general = public_users;
